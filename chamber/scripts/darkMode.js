e66fa54d1b3fe0535135f5d24dc17b3cb4a2e9d1
Bryan <bryanmt496@gmail.com>
2024-03-01 02:36:49 -0500

    Dark Mode finished!

--- chamber/scripts/darkMode.js
+++ chamber/scripts/darkMode.js
@@ -1,20 +1,26 @@
 const modeButton = document.querySelector("#mode");
 const body = document.querySelector("body");
-const textActivities = document.querySelector("h2");
+const textActivities = document.getElementsByTagName("h2");
+
 
 
 modeButton.addEventListener("click", () => {
+	var colorText = "";
 	if (modeButton.textContent.includes("🕶️")) {
 		body.style.background = "#000";
 		body.style.color = "#fff";
 		modeButton.textContent = "🔆";
-		textActivities.style.color = "#f1a208";
+		colorText = "#f1a208";
 
 	} else {
 		body.style.background = "white";
 		body.style.color = "#000";
 		modeButton.textContent = "🕶️";
+		colorText = "#003566";
 
+	}
+	for (var i=0;i<textActivities.length; i++) {
+		textActivities[i].style.color = colorText;
 	}
 
 });
