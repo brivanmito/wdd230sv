80e5c2be948ece5ab017188ed057975c3796518c
Bryan <bryanmt496@gmail.com>
2024-03-17 03:09:29 -0500

    Calculating windchill

---
+++ scripts/windchill.js
@@ -0,0 +1,31 @@
+
+
+const apiKey = '746dc8a3e0c0b3491d2565ebe7d9eb5a';
+const city = 'Salt Lake';
+
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+let temperature = document.getElementById("temperature");
+let windChill = document.getElementById("windChill");  
+
+async function getWeather() {
+  const response = await fetch(apiURL);
+  const weatherData = await response.json();
+  const temp = weatherData.main.temp;
+  const windspeed = weatherData.main.wind;
+  const tempFaren = (temp * (9/5)) + 32;
+  temperature.innerHTML = "Temperature: " + Math.round(tempFaren) + "°F";
+  getwinchill(tempFaren, windspeed);
+
+}
+
+async function getwinchill(a, b) {
+    if(a <=50 && b > 3.0){
+        const wC =   35.74 + (0.6215 * a) - (35.75 * Math.pow(b, 0.16)) + (0.4275 * a * Math.pow(b, 0.16));
+        windChill.innerHTML = "Windchill: " + Math.round(wC) + "F°";
+    } else {
+        windChill.innerHTML = "Windchill: N/A";
+    }
+}
+
+getWeather();
