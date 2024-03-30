8a3da3404fd6c14735870eb81d56f6397c2d5c1d
Bryan <bryanmt496@gmail.com>
2024-03-30 02:34:01 -0500

    Json index

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,31 @@
 const baseURL = "https://brivanmito.github.io/wdd230sv/";
 const linksURL = "https://brivanmito.github.io/wdd230sv/data/links.json";
+const listActivity = document.querySelector("#listLinks");
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data.lessons);
 }
+
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        let listItem = document.createElement('li');
+
+        listItem.textContent = `${week.lesson}: `;
+
+        week.links.forEach((link) => {
+            let linkE = document.createElement('a');
+
+            linkE.setAttribute('href', link.url);
+            linkE.setAttribute('target', '_blank');
+            linkE.textContent = link.title;
+
+        });
+        listActivity.appendChild(linkE);
+
+    });
+};
 
 getLinks();
